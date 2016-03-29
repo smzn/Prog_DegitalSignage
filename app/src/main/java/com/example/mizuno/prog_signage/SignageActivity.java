b3a4acafaf400568d193c2878e79b7aa7406be20
smzn <s.mzn.eng@gmail.com>
2016-03-29 12:53:42 +0900

    20160329 Upload

---
+++ app/src/main/java/com/example/mizuno/prog_signage/SignageActivity.java
@@ -0,0 +1,62 @@
+package com.example.mizuno.prog_signage;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SignageActivity extends AppCompatActivity {
+
+    VideoView video;
+    TextView counter;
+    String videoPath;
+    int [] videoFileName={R.raw.schedule,R.raw.iwish,R.raw.saka};//ここに動画を追加する。
+    int videoPoint = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signage);
+
+        video = (VideoView) findViewById(R.id.videoView);
+        counter = (TextView) findViewById(R.id.Counter);
+
+        videoPath = "android.resource://" + this.getPackageName() + "/" + videoFileName[videoPoint];
+        video.setVideoPath(videoPath);
+        Log.d("path", videoPath);
+        video.start();
+
+        video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+
+                videoPath = "android.resource://" + getPackageName() + "/" + videoFileName[++videoPoint % videoFileName.length];
+                video.setVideoPath(videoPath);
+                video.seekTo(0);
+                video.start();
+
+            }
+        });
+
+        //再生時間表示に関する処理
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                counter.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        counter.setText(String.valueOf((double) video.getCurrentPosition() / 1000) + "s");
+                    }
+                });
+            }
+        }, 0, 50);
+
+
+    }
+}
